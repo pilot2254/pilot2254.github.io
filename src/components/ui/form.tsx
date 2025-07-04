f85639fc3dac20c32955d6c9b33ca4ba6962627e
pilot2254 <miso.flaska@gmail.com>
2025-07-04 13:06:45 +0200

    Updated components & `npm run format`

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -3,7 +3,14 @@
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { Slot } from "@radix-ui/react-slot"
-import { Controller, ControllerProps, FieldPath, FieldValues, FormProvider, useFormContext } from "react-hook-form"
+import {
+  Controller,
+  FormProvider,
+  useFormContext,
+  type ControllerProps,
+  type FieldPath,
+  type FieldValues,
+} from "react-hook-form"
 
 import { cn } from "@/lib/utils"
 import { Label } from "@/components/ui/label"
@@ -115,7 +122,7 @@ FormDescription.displayName = "FormDescription"
 const FormMessage = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLParagraphElement>>(
   ({ className, children, ...props }, ref) => {
     const { error, formMessageId } = useFormField()
-    const body = error ? String(error?.message) : children
+    const body = error ? String(error?.message ?? "") : children
 
     if (!body) {
       return null
