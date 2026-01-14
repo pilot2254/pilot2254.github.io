628a7bcd56427b108ca3697a61fd9d7d3ddaecd2
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2026-01-14 19:36:26 +0100

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -10,14 +10,14 @@ export const siteConfig = {
     sticky: false, // toggle sticky nav here
   },
   social: {
-    twitter: "https://twitter.com/pilot2254",
-    discord: "171395713064894465",
-    reddit: "https://reddit.com/u/pilot2254",
-    steam: "https://steamcommunity.com/id/pilot2254",
-    github: "https://github.com/pilot2254/pilot2254.github.io",
-    paypal: "https://paypal.me/pilot2254",
-    kofi: "https://ko-fi.com/pilot2254",
-    btc: "bc1qr8x3k39mn7sz2l9kyk8j8293xf5spm2wsxymh9", // my actual btc address
-    eth: "0x4281F208799E24531CBE21D4d263C08E7CB68d86", // my actual eth address
+    twitter: "https://twitter.com/pilot2254",                   // twitter, even tho i dont use it
+    discord: "171395713064894465",                              // discord id, not the username
+    reddit: "https://reddit.com/u/pilot2254",                   // ah yes, let people know that im using this app full of toxic, politics-oriented people
+    steam: "https://steamcommunity.com/id/pilot2254",           // steam because why not
+    github: "https://github.com/pilot2254/pilot2254.github.io", // repo where the source code for this website is located
+    paypal: "https://paypal.me/pilot2254",                      // send me money plz
+    kofi: "https://ko-fi.com/pilot2254",                        // send me money plz
+    btc: "bc1qr8x3k39mn7sz2l9kyk8j8293xf5spm2wsxymh9",          // my actual btc address
+    eth: "0x4281F208799E24531CBE21D4d263C08E7CB68d86",          // my actual eth address
   },
 }
